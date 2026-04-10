9271cbfa83926fd36be216233d8d7d1faab6c618
Santi <santiago_abregu_@hotmail.com>
2026-04-09 21:53:04 -0300

    feat: user model

---
+++ backend/app/model/admin.py
@@ -0,0 +1,13 @@
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+from app.model.user import User
+
+
+class Admin(User):
+    __tablename__ = "admins"
+
+    id: Mapped[int] = mapped_column(ForeignKey("users.id"), primary_key=True)
+
+    __mapper_args__ = {
+        "polymorphic_identity": "admin",
+    }
