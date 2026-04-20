8a606ce1b8300508699f3aefebc35cc7430c6b06
Abril <abrilrodriguezfontana03@gmail.com>
2026-04-19 22:03:44 -0300

    property model created

---
+++ backend/app/model/property.py
@@ -0,0 +1,33 @@
+from sqlalchemy import String, Float, Enum
+from sqlalchemy.orm import Mapped, mapped_column
+from app.core.database import Base
+import enum
+
+
+class PropertyType(enum.Enum):
+    HOUSE = "house"
+    APARTMENT = "apartment"
+    LAND = "land"
+
+
+class PropertyStatus(enum.Enum):
+    AVAILABLE = "available"
+    SOLD = "sold"
+    RENTED = "rented"
+
+
+class Property(Base):
+    __tablename__ = "properties"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    property_id: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+
+    address: Mapped[str] = mapped_column(String, nullable=False)
+    location: Mapped[str] = mapped_column(String, nullable=False)
+
+    type: Mapped[PropertyType] = mapped_column(Enum(PropertyType), nullable=False)
+    price: Mapped[float] = mapped_column(Float, nullable=False)
+
+    status: Mapped[PropertyStatus] = mapped_column(Enum(PropertyStatus), nullable=False)
+
+    characteristics: Mapped[str] = mapped_column(String)
