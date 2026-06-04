cfef718edead185bcc256bbe44bfe65578ca8101
Santi <santiago_abregu_@hotmail.com>
2026-06-04 15:45:31 -0300

    Feat: seeds

--- backend/app/seeds/seed.py
+++ backend/app/seeds/seed.py
@@ -1,11 +1,15 @@
 from sqlalchemy.orm import Session
 
 from app.seeds.admin_seed import create_default_admin
+from app.seeds.client_seed import create_demo_clients
+from app.seeds.listing_seed import create_demo_listings
+from app.seeds.property_seed import create_demo_properties
+from app.seeds.real_estate_seed import create_demo_real_estates
 
 
 def run_seeds(db: Session) -> None:
     create_default_admin(db)
-    # create_users(db)
-    # create_prpoerties(db)
-    # create_listings(db)
-    # create_favs(db)
+    create_demo_clients(db)
+    create_demo_real_estates(db)
+    create_demo_properties(db)
+    create_demo_listings(db)
